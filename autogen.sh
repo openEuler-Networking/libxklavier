639e822df49ca6426c8211374330b2af287ccb5a
Sergey Udaltsov <svu@gnome.org>
2009-05-14 22:26:57 +0000

    fixing the build process

--- autogen.sh
+++ autogen.sh
@@ -89,7 +89,7 @@ do
     echo skipping $dr -- flagged as no auto-gen
   else
     echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
+    macrodirs=`sed -n -e 's,AC_CONFIG_MACRO_DIR(\(.*\)),\1,gp' < $coin`
     ( cd $dr
       aclocalinclude="$ACLOCAL_FLAGS"
       for k in $macrodirs; do
