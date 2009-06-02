c7e3f83d1c28482fe358d7f31f632c883d181596
Sergey Udaltsov <svu@gnome.org>
2009-06-02 10:40:33 +0000

    fixed potential crash

--- libxklavier/xklavier_props.c
+++ libxklavier/xklavier_props.c
@@ -374,10 +374,9 @@ xkl_config_rec_get_from_root_window_property(XklConfigRec * data,
 					       --varlen);
 					var[varlen] = '\0';
 					/* Resize the original layout */
-					((char *)
-					 g_realloc(*layout,
-						   laylen + 1))[laylen] =
-					    '\0';
+					*layout =
+					    g_realloc(*layout, laylen + 1);
+					(*layout)[laylen] = '\0';
 				}
 			}
 			layout++;
