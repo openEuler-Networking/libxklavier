47f2b9ba60f9fda2468f23923b1ba03e326cd111
Sergey Udaltsov <svu@gnome.org>
2011-01-17 23:58:30 +0000

    updated marshaling sources

--- libxklavier/xkl_engine_marshal.c
+++ libxklavier/xkl_engine_marshal.c
@@ -21,6 +21,7 @@
 #define g_marshal_value_peek_boxed(v)    g_value_get_boxed (v)
 #define g_marshal_value_peek_pointer(v)  g_value_get_pointer (v)
 #define g_marshal_value_peek_object(v)   g_value_get_object (v)
+#define g_marshal_value_peek_variant(v)  g_value_get_variant (v)
 #else /* !G_ENABLE_DEBUG */
 /* WARNING: This code accesses GValues directly, which is UNSUPPORTED API.
  *          Do not access GValues directly in your code. Instead, use the
@@ -44,6 +45,7 @@
 #define g_marshal_value_peek_boxed(v)    (v)->data[0].v_pointer
 #define g_marshal_value_peek_pointer(v)  (v)->data[0].v_pointer
 #define g_marshal_value_peek_object(v)   (v)->data[0].v_pointer
+#define g_marshal_value_peek_variant(v)  (v)->data[0].v_pointer
 #endif /* !G_ENABLE_DEBUG */
 
 
@@ -52,10 +54,10 @@
 /* INT:LONG,LONG (marshal.list:2) */
 void
 xkl_engine_INT__LONG_LONG (GClosure     *closure,
-                           GValue       *return_value,
+                           GValue       *return_value G_GNUC_UNUSED,
                            guint         n_param_values,
                            const GValue *param_values,
-                           gpointer      invocation_hint,
+                           gpointer      invocation_hint G_GNUC_UNUSED,
                            gpointer      marshal_data)
 {
   typedef gint (*GMarshalFunc_INT__LONG_LONG) (gpointer     data1,
@@ -93,10 +95,10 @@ xkl_engine_INT__LONG_LONG (GClosure     *closure,
 /* VOID:FLAGS,INT,BOOLEAN (marshal.list:3) */
 void
 xkl_engine_VOID__FLAGS_INT_BOOLEAN (GClosure     *closure,
-                                    GValue       *return_value,
+                                    GValue       *return_value G_GNUC_UNUSED,
                                     guint         n_param_values,
                                     const GValue *param_values,
-                                    gpointer      invocation_hint,
+                                    gpointer      invocation_hint G_GNUC_UNUSED,
                                     gpointer      marshal_data)
 {
   typedef void (*GMarshalFunc_VOID__FLAGS_INT_BOOLEAN) (gpointer     data1,
